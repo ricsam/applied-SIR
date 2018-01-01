229db20d29f31ce3d0edf6bf931aa4812050c4c1
Richard Samuelsson <noobtoothfairy@gmail.com>
2018-01-01 20:51:50 +0100

    update graphs

--- src/levenberg-marquardt-modified.js
+++ src/levenberg-marquardt-modified.js
@@ -1,38 +1,32 @@
 import { Matrix, inverse as inv } from 'ml-matrix';
 
-const stepSize = 1e-3;
+const stepSize = 0.5;
 
-const alpha = 0.01;
-const beta = 0.75;
+const beta = 0.2;
 const gamma = 0.15;
 const v = 0.15;
-
+const alpha = 0.01;
 
 export default class LMm {
-  damping = 1;
+  damping = 0.8;
   gradientDifference = 1e-2;
-  maxIterations = 25;
-  errorTolerance = 1e-2 * 4;
+  maxIterations = 5;
+  errorTolerance = 1e-4 * 4;
   systemSize = 4;
 
   stepSize = stepSize;
   frameIterations = Math.round(1 / stepSize);
-
-  s0 = 0.9;
-  i0 = 0.1;
-  p0 = 0;
-  d0 = 0;
 
   alpha = alpha;
   beta = beta;
   gamma = gamma;
   v = v;
 
-  resetArgs = true;
+  resetArgs = false;
 
   curves = ['suceptible', 'infected', 'immune', 'dead'];
   params = ['s', 'i', 'p', 'd'];
-  args = ['beta', 'gamma', 'alpha', 'v'];
+  args = ['beta', 'gamma', 'v', 'alpha'];
 
   ds(t, { s, i, p }) {
     return -this.beta * s * i + this.gamma * p;
@@ -92,14 +86,20 @@ export default class LMm {
     this.discreteSavedEuler = [];
 
     const initialData = {
-      s: this.s0, i: this.i0, p: this.p0, d: this.d0,
+      s: this.s0,
+      i: this.i0,
+      p: this.p0,
+      d: this.d0,
     };
 
-    const s = this.s0 * this.ds(0, initialData);
-    const i = this.i0 * this.di(0, initialData);
-    const p = this.p0 * this.dp(0, initialData);
-    const d = this.d0 * this.dd(0, initialData);
+    const s = this.s0 + this.stepSize * this.ds(0, initialData);
+    const i = this.i0 + this.stepSize * this.di(0, initialData);
+    const p = this.p0 + this.stepSize * this.dp(0, initialData);
+    const d = this.d0 + this.stepSize * this.dd(0, initialData);
 
+    if (s < 0 || i < 0 || p < 0 || d < 0) {
+      debugger;
+    }
 
     if (
       Number.isNaN(s) ||
@@ -135,25 +135,45 @@ export default class LMm {
     this.nextV = this.v + this.gradientDifference;
 
     const initialData = {
-      s: this.s0, i: this.i0, p: this.p0, d: this.d0,
+      s: this.s0,
+      i: this.i0,
+      p: this.p0,
+      d: this.d0,
     };
 
-    const s = this.s0 * this.ds(0, initialData);
-    const i = this.i0 * this.di(0, initialData);
-    const p = this.p0 * this.dp(0, initialData);
-    const d = this.d0 * this.dd(0, initialData);
+    const s = this.s0 + this.stepSize * this.ds(0, initialData);
+    const i = this.i0 + this.stepSize * this.di(0, initialData);
+    const p = this.p0 + this.stepSize * this.dp(0, initialData);
+    const d = this.d0 + this.stepSize * this.dd(0, initialData);
 
-    const nextBetaS = this.s0 * this.nextBetaDs(0, initialData);
-    const nextBetaI = this.i0 * this.nextBetaDi(0, initialData);
+    const nextBetaS = this.s0 + this.stepSize * this.nextBetaDs(0, initialData);
+    const nextBetaI = this.i0 + this.stepSize * this.nextBetaDi(0, initialData);
 
-    const nextGammaS = this.s0 * this.nextGammaDs(0, initialData);
-    const nextGammaP = this.p0 * this.nextGammaDp(0, initialData);
+    const nextGammaS =
+      this.s0 + this.stepSize * this.nextGammaDs(0, initialData);
+    const nextGammaP =
+      this.p0 + this.stepSize * this.nextGammaDp(0, initialData);
 
-    const nextAlphaI = this.i0 * this.nextAlphaDi(0, initialData);
-    const nextAlphaD = this.d0 * this.nextAlphaDd(0, initialData);
+    const nextAlphaI =
+      this.i0 + this.stepSize * this.nextAlphaDi(0, initialData);
+    const nextAlphaD =
+      this.d0 + this.stepSize * this.nextAlphaDd(0, initialData);
 
-    const nextVI = this.i0 * this.nextVDi(0, initialData);
-    const nextVP = this.p0 * this.nextVDp(0, initialData);
+    const nextVI = this.i0 + this.stepSize * this.nextVDi(0, initialData);
+    const nextVP = this.p0 + this.stepSize * this.nextVDp(0, initialData);
+
+    if (
+      nextBetaS < 0 ||
+      nextBetaI < 0 ||
+      nextGammaS < 0 ||
+      nextGammaP < 0 ||
+      nextAlphaI < 0 ||
+      nextAlphaD < 0 ||
+      nextVI < 0 ||
+      nextVP < 0
+    ) {
+      debugger;
+    }
 
     if (
       Number.isNaN(s) ||
@@ -236,7 +256,7 @@ export default class LMm {
       const gradientDiffSum =
         this.discreteSavedEuler[0][paramKey] - innerGradientSum / 4;
 
-      this.gradientValues[i][0] = gradientDiffSum / 4; // average
+      this.gradientValues[i][0] = gradientDiffSum; // average
 
       const matrixDiff =
         this.yData[0][dataKey] - this.discreteSavedEuler[0][paramKey];
@@ -265,17 +285,50 @@ export default class LMm {
         const p = prevValues.p + this.stepSize * this.dp(ts, prevValues);
         const d = prevValues.d + this.stepSize * this.dd(ts, prevValues);
 
-        const nextBetaS = nextPrevValues.beta.s + this.stepSize * this.nextBetaDs(ts, nextPrevValues.beta);
-        const nextBetaI = nextPrevValues.beta.i + this.stepSize * this.nextBetaDi(ts, nextPrevValues.beta);
+        const nextBetaS =
+          nextPrevValues.beta.s +
+          this.stepSize * this.nextBetaDs(ts, nextPrevValues.beta);
+        const nextBetaI =
+          nextPrevValues.beta.i +
+          this.stepSize * this.nextBetaDi(ts, nextPrevValues.beta);
 
-        const nextGammaS = nextPrevValues.gamma.s + this.stepSize * this.nextGammaDs(ts, nextPrevValues.gamma);
-        const nextGammaP = nextPrevValues.gamma.p + this.stepSize * this.nextGammaDp(ts, nextPrevValues.gamma);
+        const nextGammaS =
+          nextPrevValues.gamma.s +
+          this.stepSize * this.nextGammaDs(ts, nextPrevValues.gamma);
+        const nextGammaP =
+          nextPrevValues.gamma.p +
+          this.stepSize * this.nextGammaDp(ts, nextPrevValues.gamma);
 
-        const nextAlphaI = nextPrevValues.alpha.i + this.stepSize * this.nextAlphaDi(ts, nextPrevValues.alpha);
-        const nextAlphaD = nextPrevValues.alpha.d + this.stepSize * this.nextAlphaDd(ts, nextPrevValues.alpha);
+        const nextAlphaI =
+          nextPrevValues.alpha.i +
+          this.stepSize * this.nextAlphaDi(ts, nextPrevValues.alpha);
+        const nextAlphaD =
+          nextPrevValues.alpha.d +
+          this.stepSize * this.nextAlphaDd(ts, nextPrevValues.alpha);
 
-        const nextVI = nextPrevValues.v.i + this.stepSize * this.nextVDi(ts, nextPrevValues.v);
-        const nextVP = nextPrevValues.v.p + this.stepSize * this.nextVDp(ts, nextPrevValues.v);
+        const nextVI =
+          nextPrevValues.v.i +
+          this.stepSize * this.nextVDi(ts, nextPrevValues.v);
+        const nextVP =
+          nextPrevValues.v.p +
+          this.stepSize * this.nextVDp(ts, nextPrevValues.v);
+
+        if (
+          s < 0 ||
+          i < 0 ||
+          p < 0 ||
+          d < 0 ||
+          nextBetaS < 0 ||
+          nextBetaI < 0 ||
+          nextGammaS < 0 ||
+          nextGammaP < 0 ||
+          nextAlphaI < 0 ||
+          nextAlphaD < 0 ||
+          nextVI < 0 ||
+          nextVP < 0
+        ) {
+          debugger;
+        }
         if (
           Number.isNaN(s) ||
           s === Infinity ||
@@ -360,7 +413,7 @@ export default class LMm {
       }
       const gradientDiffSum = result[paramKey] - innerGradientSum / 4;
 
-      this.gradientValues[i][time] = gradientDiffSum / 4; // average
+      this.gradientValues[i][time] = gradientDiffSum; // average
 
       const matrixDiff = this.yData[time][curveKey] - result[paramKey];
       matrixDiffSum += matrixDiff;
@@ -380,28 +433,6 @@ export default class LMm {
   setData(yData, size) {
     this.yData = yData;
     this.dataSetLength = size;
-  }
-  step() {
-    const identity = Matrix.eye(4).mul(this.damping * this.gradientDifference ** 2);
-
-    const gradientFunc = this.gradientFunction();
-
-    const matrixFunc = this.matrixFunction().transposeView();
-    const inverse = inv(identity.add(gradientFunc.mmul(gradientFunc.transposeView())));
-
-    let params = new Matrix([[this.beta, this.gamma, this.alpha, this.v]]);
-    params = params.sub(inverse
-      .mmul(gradientFunc)
-      .mmul(matrixFunc)
-      .mul(this.gradientDifference)
-      .transposeView());
-
-    const arr = params.to1DArray();
-
-
-    [this.beta, this.gamma, this.alpha, this.v] = arr;
-  }
-  interate() {
     if (this.resetArgs) {
       this.resultingArgs = [this.beta, this.gamma, this.alpha, this.v];
       this.alpha = alpha;
@@ -409,6 +440,40 @@ export default class LMm {
       this.gamma = gamma;
       this.v = v;
     }
+    this.s0 = this.yData[0].suceptible;
+    this.i0 = this.yData[0].infected;
+    this.p0 = this.yData[0].immune;
+    this.d0 = this.yData[0].dead;
+  }
+  step() {
+    const identity = Matrix.eye(4).mul(
+      this.damping * this.gradientDifference ** 2
+    );
+
+    const gradientFunc = this.gradientFunction();
+
+    const matrixFunc = this.matrixFunction().transposeView();
+    const inverse = inv(
+      identity.add(gradientFunc.mmul(gradientFunc.transposeView()))
+    );
+
+    let params = new Matrix([[this.beta, this.gamma, this.alpha, this.v]]);
+    params = params.sub(
+      inverse
+        .mmul(gradientFunc)
+        .mmul(matrixFunc)
+        .mul(this.gradientDifference)
+        .transposeView()
+    );
+
+    const arr = params.to1DArray();
+
+    // console.log(this.gradientValues, this.matrixValues);
+
+    [this.beta, this.gamma, this.alpha, this.v] = arr;
+  }
+  interate() {
+    this.error = 0;
     const res1 = this.initialTargetEuler();
     const res2 = this.nextInitialTargetEuler();
     if (!res1 || !res2) {
@@ -425,12 +490,13 @@ export default class LMm {
     if (this.damping <= 0) {
       throw new Error('The damping option must be a positive number');
     } else if (!Array.isArray(this.yData) || this.dataSetLength < 2) {
-      throw new Error('The data parameter elements must be an array with more than 2 points');
+      throw new Error(
+        'The data parameter elements must be an array with more than 2 points'
+      );
     }
 
     let res = this.interate();
     let converged = this.error <= this.errorTolerance || !res;
-
 
     let iteration;
     for (
@@ -444,10 +510,17 @@ export default class LMm {
     }
 
     // console.log(this.error, iteration);
-    console.log(JSON.stringify({
-      iterations: iteration,
-      error: this.error,
-      args: this.resetArgs ? this.resultingArgs : [this.beta, this.gamma, this.alpha, this.v]}, true, 2));
+    console.log(
+      JSON.stringify(
+        {
+          iterations: iteration,
+          error: this.error,
+          args: [this.beta, this.gamma, this.alpha, this.v],
+        },
+        true,
+        2
+      )
+    );
 
     return {
       parameterValues: [this.beta, this.gamma, this.alpha, this.v],
@@ -466,22 +539,21 @@ export default class LMm {
   }
 
   /* eslint no-param-reassign: ["error", { "props": false }] */
-  plot({
-    context: c, width, height,
-  }) {
-    if (! this.discreteSavedEuler || ! this.discreteSavedEuler.length) return;
+  plot({ context: c, width, height, elapsedTime }) {
+    if (!this.discreteSavedEuler || !this.discreteSavedEuler.length) return;
     for (let k = 0; k < 4; k += 1) {
       c.strokeStyle = 'orange';
-      c.lineWidth = 5;
+      c.lineWidth = 3;
 
-      const initialValue = height * (1 - this.discreteSavedEuler[0][this.params[k]]);
+      const initialValue =
+        height * (1 - this.discreteSavedEuler[0][this.params[k]]);
 
       c.beginPath();
       c.moveTo(0, initialValue);
 
       for (let i = 1; i < this.discreteSavedEuler.length; i += 1) {
         const yVal = this.discreteSavedEuler[i][this.params[k]];
-        const vX = k * width / this.discreteSavedEuler.length;
+        const vX = i * width / elapsedTime;
         const vY = height * (1 - yVal);
         c.lineTo(vX, vY);
       }
