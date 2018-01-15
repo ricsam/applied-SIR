9100ed94e53cdab5efb1054d078bf5daf77a7110
Montop <Montop@gmail.com>
2018-01-15 17:36:21 +0100

    finale

--- matlab/simulationfinal.m
+++ matlab/simulationfinal.m
@@ -1,20 +1,21 @@
 clear; clear all; clc;
 
-global this_width this_height this_maxSpeed this_diseaseRadius this_baseInfectionDuration this_baseImmunityDuration this_personRadius this_numberOfPeople this_probabilityOfBecommingImmuneElseDie f initialNumberOfInfected;
+global this_width this_height this_maxSpeed this_diseaseRadius this_baseInfectionDuration this_baseImmunityDuration this_personRadius this_numberOfPeople this_probabilityOfBecommingImmuneElseDie f initialNumberOfInfected; % Makes variables global.
 
-this_width = 800;
-this_height = 600;
-this_maxSpeed = 1;
-this_diseaseRadius = 10;
-this_baseInfectionDuration = 15;
-this_baseImmunityDuration = 360;
-this_personRadius = 3;
-this_numberOfPeople = 1200;
-this_probabilityOfBecommingImmuneElseDie = 0.8;
-initialNumberOfInfected = 5;
+this_width = 800; % Width of playfield.
+this_height = 600; % Height of playfield.
+this_maxSpeed = 1; % Maximum movement distance of agents per iteration.
+this_diseaseRadius = 10; % Maximum distance of disease spread.
+this_baseInfectionDuration = 15; % Duration of disease.
+this_baseImmunityDuration = 15; % Duration of immunity.
+this_personRadius = 3; % Size of agents.
+this_numberOfPeople = 3000; % Amount of agents in simulation.
+this_probabilityOfBecommingImmuneElseDie = 0.8; % Fairly self-explanatory.
+initialNumberOfInfected = 5; % Initial amount of infected agents.
 
-[newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, newDeathMatrix] = getInitialFrame();
+[newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, newDeathMatrix] = getInitialFrame(); % Initialises the simulation.
 
+% Main program loop.
 f = figure;
 while true
   render(newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, newDeathMatrix);
@@ -22,10 +23,11 @@ while true
   pause(1/60);
 end
 
+% Rendering function.
 function ret = renderMatrix(matrix, newXPositions, newYPositions, c)
 
-    xPositions = []; % Make empty list for x-coordinates.
-    yPositions = []; % Make empty list for x-coordinates.
+    xPositions = []; % Makes empty list for x-coordinates.
+    yPositions = []; % Makes empty list for x-coordinates.
     for u=1:size(matrix, 2) % Iterates through matrix for the following frame.
         if matrix(1, u) > 0
           xPositions(end + 1) = newXPositions(1, u); % Sets every present person to 1.
@@ -35,36 +37,29 @@ function ret = renderMatrix(matrix, newXPositions, newYPositions, c)
 
     scatter(xPositions(:),yPositions(:),10,c,"filled"); % Plots the filtered coordinates as points.
     hold on;
-    set(gca,'color','black');
+    set(gca,'color','black'); % Makes background colour black for improved visibility.
 end
 
+% Function that makes the different groups of agents more friendly for rendering.
 function ret = render(newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, newDeathMatrix)
     global f;
-    clf(f);
-    scatter(newXPositions(:), newYPositions(:), 10, 'yellow', 'filled');
+    clf(f); % Clear figure window.
+    scatter(newXPositions(:), newYPositions(:), 10, 'yellow', 'filled'); % Plots susceptible agents.
     hold on;
 
-    renderMatrix(newInfectionMatrix, newXPositions, newYPositions, 'red');
-    renderMatrix(newDeathMatrix, newXPositions, newYPositions, 'white');
-    renderMatrix(newImmunityMatrix, newXPositions, newYPositions, 'green');
-
-    %scatter(recovered(:,1),recovered(:,2),10,'b',"filled");
-    %hold on
-    %scatter(infected(:,1),infected(:,2),10,'g',"filled");
-    %hold on
+    renderMatrix(newInfectionMatrix, newXPositions, newYPositions, 'red'); % Plots infected agents.
+    renderMatrix(newDeathMatrix, newXPositions, newYPositions, 'white'); % Plots dead agents.
+    renderMatrix(newImmunityMatrix, newXPositions, newYPositions, 'green'); % Plots immune agents.
 end
 
-
-
-
-
+% Initialisation function.
 function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, newDeathMatrix] = getInitialFrame()
   global this_width this_height this_maxSpeed this_diseaseRadius this_baseInfectionDuration this_baseImmunityDuration this_personRadius this_numberOfPeople this_probabilityOfBecommingImmuneElseDie initialNumberOfInfected;
 
-  xPositions = [];
-  yPositions = [];
-  immunityMatrix = []; % time ticking, of beeing immune
-  infectionMatrix = []; % time ticking down, of infection
+  xPositions = []; % Initial x-position array.
+  yPositions = []; % Initial y-position array.
+  immunityMatrix = []; % Time ticking, of being immune.
+  infectionMatrix = []; % Time ticking down, of infection.
   deathMatrix = [];
 
   for i = 1:this_numberOfPeople - initialNumberOfInfected
@@ -76,7 +71,7 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
   end
 
   for i = 1:initialNumberOfInfected
-    % push one infected
+    % Push one infected.
     xPositions(end + 1) = getRandomArbitrary(0, this_width);
     yPositions(end + 1) = getRandomArbitrary(0, this_height);
     immunityMatrix(end + 1) = 0;
@@ -91,6 +86,7 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
   newDeathMatrix = deathMatrix;
 end
 
+% Calculates probability of disease spreading.
 function cb = effectFuntion(radius)
   global this_diseaseRadius this_personRadius;
   if radius == 0
@@ -112,17 +108,19 @@ function cb = bindY(y)
   cb = min(max(y, 0), this_height);
 end
 
+% Obtains positions and states of agents.
 function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, newDeathMatrix] = update(xPositions, yPositions, infectionMatrix, immunityMatrix, deathMatrix)
 
   global this_width this_height this_maxSpeed this_diseaseRadius this_baseInfectionDuration this_baseImmunityDuration this_personRadius this_numberOfPeople this_probabilityOfBecommingImmuneElseDie;
 
-  newXPositions = [];
-  newYPositions = [];
-  newDeathMatrix = [deathMatrix];
+  newXPositions = []; % Matrix for updated x-positions.
+  newYPositions = []; % Matrix for updated y-positions.
+  newDeathMatrix = [deathMatrix]; % Updated matrix of dead agents.
 
-  xPositions_length = size(xPositions, 2);
+  xPositions_length = size(xPositions, 2); % Determines size of x-position matrix.
+  % Determines if an agent is dead.
   for i = 1:xPositions_length
-    if newDeathMatrix(i)
+    if newDeathMatrix(i) 
       newXPositions(i) = xPositions(i);
       newYPositions(i) = yPositions(i);
       continue;
@@ -130,13 +128,14 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
 
     x = xPositions(i);
     y = yPositions(i);
-    winkel = getRandomArbitrary(0, 2 * pi);
-    speed = getRandomArbitrary(0, this_maxSpeed);
-    newXPositions(i) = bindX(speed * cos(winkel) + x);
+    winkel = getRandomArbitrary(0, 2 * pi); % Determines angle of movement.
+    speed = getRandomArbitrary(0, this_maxSpeed); % Determines distance of movement.
+    newXPositions(i) = bindX(speed * cos(winkel) + x); 
     newYPositions(i) = bindY(speed * sin(winkel) + y);
   end
 
-  newImmunityMatrix = [];
+  newImmunityMatrix = []; % Matrix of immune agents.
+  % Determines if an agent is immune.
   for i = 1:size(immunityMatrix, 2)
     if immunityMatrix(i) > 0
       newImmunityMatrix(i) = immunityMatrix(i) - 1;
@@ -145,39 +144,36 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
     end
   end
 
-  % console.log(newXPositions, newYPositions);
-
-  mightGetInfected = [];
+  mightGetInfected = []; % Persons who are at risk of becoming infected.
   mightGetInfected_index = [];
   mightGetInfected_distances = [];
   newInfectionMatrix = [];
 
-  % copy death matrix */
   for u = 1:size(infectionMatrix, 2)
     rootX = newXPositions(u);
     rootY = newYPositions(u);
 
-    % special case as someone will become non-infeced after this */
-    if infectionMatrix(u) == 1
-      probability = getRandomArbitrary(0, 1);
-      if probability <= this_probabilityOfBecommingImmuneElseDie
+    if infectionMatrix(u) == 1 % Checks if person at position is about to stop being infected.
+      probability = getRandomArbitrary(0, 1); % Generates a random number for probability check.
+      if probability <= this_probabilityOfBecommingImmuneElseDie % If the generated number is lower than death rate, person becomes immune.
         newImmunityMatrix(u) = this_baseImmunityDuration;
-      else
+      else % Person dies.
         newDeathMatrix(u) = true;
       end
     end
 
-    % update the infection matrix */
+    % Update the infection matrix.
     if infectionMatrix(u) > 0
       newInfectionMatrix(u) = infectionMatrix(u) - 1;
     else
       newInfectionMatrix(u) = infectionMatrix(u);
     end
 
+    % Sort agents that are at risk of infection.
     if newInfectionMatrix(u) ~= 0 && newDeathMatrix(u) ~= true
       for i = 1:size(newXPositions, 2)
         if i == u || newImmunityMatrix(i) > 0 || newDeathMatrix(i)
-          continue; % eslint-disable-line no-continue
+          continue;
         end
 
         x = newXPositions(i);
@@ -199,6 +195,7 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
               alreadyMightInfected = true;
             end
           end
+
           if ~alreadyMightInfected
             mightGetInfected(end + 1) = size(mightGetInfected, 2) + 1;
             mightGetInfected_index(end + 1) = i;
@@ -212,6 +209,7 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
     end
   end
 
+  % Checks if agent becomes infected.
   for i = 1:size(mightGetInfected, 2)
     distances = mightGetInfected_distances(:, i);
     distances = distances(distances'~=0)';
@@ -242,6 +240,7 @@ function [newXPositions, newYPositions, newInfectionMatrix, newImmunityMatrix, n
 
 end
 
+% Function for random numbers.
 function cb = getRandomArbitrary(min, max)
   bound = rand() * (max - min);
   cb = bound + min;
